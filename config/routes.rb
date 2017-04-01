0e12cb9ea9b1680db4511a5fe8861e662bdb8da7
Justin Homan <justinhoman@hotmail.com>
2017-04-01 22:27:19 +0200

    Controllers voor de winkelwagen aangemaakt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+
   resources :pages
   get '/hosting', to: 'pages#hosting'
   get '/cloud', to: 'pages#cloud'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create! id: 1, name: "Hosting Brons", price: 5.95, active:true
+Product.create! id: 2, name: "Hosting Silver", price: 7.95, active:true
+Product.create! id: 3, name: "Hosting Goud", price: 10.95, active:true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In behandeling"
+OrderStatus.create! id: 2, name: "Geplaatst"
+OrderStatus.create! id: 3, name: "Geleverd"
+OrderStatus.create! id: 4, name: "Geannuleerd"
