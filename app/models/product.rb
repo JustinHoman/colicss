dff32fea638c3d76ae64477f2e4fa891fbb3724c
Justin Homan <justinhoman@hotmail.com>
2017-04-02 00:12:08 +0200

    Winkelwagen rommel verwijderd

--- app/models/product.rb
+++
@@ -1,5 +0,0 @@
-class Product < ApplicationRecord
-  has_many :order_items
-
-  default_scope { where(active: true) }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
-  get 'order_items/create'
-
-  get 'order_items/update'
-
-  get 'order_items/destroy'
-
-  get 'carts/show'
-
-  get 'products/index'
 
   resources :pages, only: [:index, :hosting, :cloud, :social, :contact]
-  resources :products, only: [:index]
-  resources :carts, only: [:show]
-  resources :order_items, only: [:create, :update, :destroy]
   get '/hosting', to: 'pages#hosting'
   get '/cloud', to: 'pages#cloud'
   get '/reviews', to: 'pages#reviews'
   get '/social', to: 'pages#social'
   root 'pages#index'
-
-
-  get '/cart', to: 'carts#show'
 end
