c7117d4ee43adcadbe0f64866bda0024afcadf53
Justin Homan <justinhoman@hotmail.com>
2017-04-02 12:44:16 +0200

    db migrations verwijderd

--- db/migrate/20170401224241_rename_type.rb
+++
@@ -1,5 +0,0 @@
-class RenameType < ActiveRecord::Migration[5.0]
-  def up
-    t.type :string
-  end
-end
--- db/migrate/20170401224522_rename_type1.rb
+++
@@ -1,5 +0,0 @@
-class RenameType1 < ActiveRecord::Migration[5.0]
-  def up
-    rename_column :products, :type, :product_type
-  end
-end
